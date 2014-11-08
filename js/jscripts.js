040f80688e8efadbe12bbc427fce7feffe4928b3
Brandon Littell <brandonl3737@gmail.com>
2014-11-08 17:06:54 -0500

    Removed old JS files, pulled JS logic out of index.html.

--- js/jscripts.js
+++ js/jscripts.js
@@ -1,47 +1,27 @@
-﻿/*var $cache = $('.stickyPanel');
-var navHeight = $('.top-bar').height() + 20;
-var distance = $cache.offset().top;
-var scrollThreshold = distance - navHeight;
+﻿$(document).ready(function() {
 
-function fixDiv() {
-    
-
-
-
-    if ($cache.length > 0) {
-        //var distance = $cache.length ? $cache.offset().top : 0,
-        //$window = $(window);
-        
-        //alert(navHeight + "   " + distance + "  " + scrollThreshold);
-        //alert(distance);
-        //var offst = this.outerHeight($('.top-bar'));
-       // var offst = 0;
-
-        if ($(window).scrollTop() >= scrollThreshold ) {
-            $cache.css({ 'position': 'fixed', 'top': navHeight });
-            //$('body').css('padding-top', offst);
-        }
-        else {
-            $cache.css({ 'position': 'relative', 'top': 'auto' });
-            //alert('Shiiiiiiiiiit');
-            //$('body').css('padding-top', '0');
-        }
-    }
-}
-$(window).scroll(fixDiv);
-fixDiv();
-*/
-
-$(document).ready(function() {
+    // Navigation fade
     $(window).on('scroll', function() {
         var top = $(window).scrollTop();
-        var scrollThreshold = $('#welcome').height();
+        var scrollThreshold = $('#welcome').height() - $('#navigation').height();
         if(top >= scrollThreshold) {
             $('#navigation').toggleClass('alt-colors', true);
         }
         else {
             $('#navigation').toggleClass('alt-colors', false);
         }
+    });
+
+    $('#nav-menu').click( function(e){
+        e.preventDefault();
+        $('#offcanvas-navigation').toggleClass( 'on' );
+        $('#body-shade').toggleClass('on');
+    });
+
+    $('#body-shade').click( function(e){
+        e.preventDefault();
+        $('#offcanvas-navigation').toggleClass( 'on' );
+        $('#body-shade').toggleClass('on');
     });
 
     $(window).scrollTop();
