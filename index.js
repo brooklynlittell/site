e423dc28b5769bb12aa5183956aac9ce93265f3e
Brandon Littell <brandon@brandonlittell.com>
2018-01-11 22:16:18 -0800

    Display simplified index

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 var http = require('http');
+var fs = require("fs");
 
 var server = http.createServer(function(request, response) {
 
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
+    fs.readFile("index.html", function(err, data){
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.write(data);
+        response.end();
+      });
+    //response.writeHead(200, {"Content-Type": "text/plain"});
+    //response.end("Hello World!");
 
 });
 
