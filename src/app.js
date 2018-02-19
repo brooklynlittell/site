69faeb1e136d29b20d8ed5502c967715b4be6487
Brandon Littell <brandon@brandonlittell.com>
2018-02-19 12:28:49 -0800

    Add gulp file, display static content

--- src/app.js
+++ src/app.js
@@ -1,26 +1,8 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'));
+//app.get('/', (req, res) => res.send('Hello World!'));
+app.use(express.static('build'));
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
-
-// var http = require('http');
-// var fs = require("fs");
-
-// var server = http.createServer(function(request, response) {
-
-//     fs.readFile("index.html", function(err, data){
-//         response.writeHead(200, {'Content-Type': 'text/html'});
-//         response.write(data);
-//         response.end();
-//       });
-//     //response.writeHead(200, {"Content-Type": "text/plain"});
-//     //response.end("Hello World!");
-
-// });
-
-// var port = process.env.PORT || 1337;
-// server.listen(port);
-
-// console.log("Server running at http://localhost:%d", port);
+var port = process.env.PORT || 3000;
+app.listen(port, () => console.log("Server running at http://localhost:%d", port));
