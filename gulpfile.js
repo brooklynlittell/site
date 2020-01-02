dc032006371356672d112efc0c9512ac01bfee6e
Brandon Littell <brandon@brandonlittell.com>
2020-01-01 16:16:42 -0800

    Expand gulpfile to support auto reload

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,70 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
 var minifyCSS = require('gulp-csso');
+var log = require('fancy-log');
+var browserSync = require('browser-sync').create();
 
-gulp.task('html', function() {
+// Remove pre-existing content from output folders
+var cleanDist = function(done) {
+
+	// Clean the dist folder
+	// del.sync([
+	// 	'build/'
+	// ]);
+
+	return done();
+};
+
+var buildHtml = function(done) {
     return gulp.src('src/views/*.html')
         .pipe(gulp.dest('build'))
-});
+}
 
-gulp.task('css', function() {
+var buildStyles = function(done) {
     return gulp.src('src/views/css/*.less')
         .pipe(less())
         .pipe(minifyCSS())
         .pipe(gulp.dest('build/css'))
-});
+}
 
-gulp.task('default', gulp.series('html', 'css'));
+var startServer = function(done) {
+	browserSync.init({
+		server: {
+			baseDir: 'build/'
+		}
+	});
+	done();
+};
+
+// Reload the browser when files change
+var reloadBrowser = function (done) {
+	browserSync.reload();
+	done();
+};
+
+var watchSource = function (done) {
+	gulp.watch('src/**/*.*', gulp.series(exports.default, reloadBrowser));
+	done();
+};
+
+/**
+ * Export Tasks
+ */
+
+// Default task
+// gulp
+exports.default = gulp.series(
+    cleanDist,
+    gulp.parallel(
+        buildHtml,
+        buildStyles
+    )
+);
+
+// Watch and reload
+// gulp watch
+exports.watch = gulp.series(
+    exports.default,
+    startServer,
+    watchSource
+);
--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 
-//app.get('/', (req, res) => res.send('Hello World!'));
 app.use('/', express.static('build'));
 
 var port = process.env.PORT || 3000;
